0c43e46c445eef4f55f21631f90221e680e13a7a
dane <dlabue@umail.iu.edu>
2013-05-26 19:49:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KatiesGrmApp::Application.config.secret_token = '46316dcfcd99a1bdad3b239aeb515b3ae95bbce5accb33d94bf1eed80c94d1853f3d81b4fbe477cdae5224643957657f5c3b7382628d7c4b742deaaf225ccdc9'
