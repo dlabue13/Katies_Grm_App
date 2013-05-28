54ba07f9b28c4a4bc7f91fca85f18eed1cc84eb5
dane <dlabue@umail.iu.edu>
2013-05-27 20:52:52 -0400

    Change Root

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ KatiesGrmApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'grm_users#index'
+  root :to => 'grm_users#index'
 
   # See how all your routes lay out with "rake routes"
 
