0d30438f3364c6b115065a10813447f944a003b9
dane <dlabue@umail.iu.edu>
2013-06-13 21:12:38 -0400

    Test Pic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602162825) do
+ActiveRecord::Schema.define(:version => 20130614003015) do
 
   create_table "a_users", :force => true do |t|
     t.string   "name"
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(:version => 20130602162825) do
     t.string   "gr_style"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "grm_pics", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "grm_stylists", :force => true do |t|
