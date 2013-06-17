36178f68abfcfcbd10aa8790b9491dd50c611d65
dane <dlabue@umail.iu.edu>
2013-06-17 19:51:35 -0400

    Test Pic

---
+++ app/helpers/grm_pics_helper.rb
@@ -0,0 +1,2 @@
+module GrmPicsHelper
+end
--- db/migrate/20130616232750_create_grm_pics.rb
+++ db/migrate/20130616232750_create_grm_pics.rb
@@ -2,7 +2,7 @@ class CreateGrmPics < ActiveRecord::Migration
   def change
     create_table :grm_pics do |t|
       t.string :name
-      t.text :description
+      t.string :description
       t.string :image
 
       t.timestamps
