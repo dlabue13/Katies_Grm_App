696e49649e6f387f5b77d2a2da2a4059f4a384b1
dane <dlabue@umail.iu.edu>
2013-06-17 22:44:21 -0400

    Fix Pic

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
    include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+   include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
