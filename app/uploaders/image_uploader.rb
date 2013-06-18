b68c99bff917138a8b6d69a09b545c753a7f0bf6
dane <dlabue@umail.iu.edu>
2013-06-17 22:22:55 -0400

    Testing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,8 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  include ActionView::AssetPaths
+  include ActionView::Helpers::AssetTagHelper
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
