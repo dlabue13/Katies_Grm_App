ddbb1d39c47f36f1a3a6a4cd058ce72aa96da21b
dane <dlabue@umail.iu.edu>
2013-07-08 19:20:19 -0400

    Working on ajax

--- app/controllers/grm_pics_controller.rb
+++ app/controllers/grm_pics_controller.rb
@@ -44,11 +44,12 @@ class GrmPicsController < ApplicationController
 
     respond_to do |format|
       if @grm_pic.save
-        format.html { redirect_to @grm_pic, notice: 'Grm pic was successfully created.' }
-        format.json { render json: @grm_pic, status: :created, location: @grm_pic }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @grm_pic.errors, status: :unprocessable_entity }
+        format.js
+#        format.html { redirect_to @grm_pic, notice: 'Grm pic was successfully created.' }
+#        format.json { render json: @grm_pic, status: :created, location: @grm_pic }
+#      else
+#        format.html { render action: "new" }
+#        format.json { render json: @grm_pic.errors, status: :unprocessable_entity }
       end
     end
   end
