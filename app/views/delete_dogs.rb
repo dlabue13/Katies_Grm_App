b7e7e0a9c85cef588ac1d0ab47d1cdbba3dd42ef
dane <dlabue@umail.iu.edu>
2013-08-26 15:40:23 -0400

    Test Delete Dialog

--- app/views/delete_dogs.rb
+++ app/views/delete_dogs.rb
@@ -1,11 +1,11 @@
-<div data-role="page" id="delete_grmappt">
+<div data-role="page" id="delete_dog">
   <div data-role="header">
     <h1>Delete Appt</h1>
   </div>
   <div data-role="content">
-    <h1 style="text-align: center;">Delete Appointment</h1>
+    <h1 style="text-align: center;">Delete Dog</h1>
     <p style="text-align: center;">Are you sure?</a></p>
-    <p><%= button_to "Delete", @grm_grappt, :method => :delete %></p>
-    <p><a href="grm_grappt" data-role="button" data-rel="back">Cancel</a></p>
+    <p><%= button_to "Delete", @grm_dog, :method => :delete %></p>
+    <p><a href="grm_dog" data-role="button" data-rel="back">Cancel</a></p>
   </div>
 </div>
