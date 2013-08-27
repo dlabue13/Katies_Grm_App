8c21c6f413b340e4eb9a8e98891ec5d0bab1cf7a
dane <dlabue@umail.iu.edu>
2013-08-27 17:06:20 -0400

    Update delete controller

--- app/controllers/deletes_controller.rb
+++ app/controllers/deletes_controller.rb
@@ -1,4 +1,13 @@
 class DeletesController < ApplicationController
+  # DELETE /grm_dogs/1
+  # DELETE /grm_dogs/1.json
   def destroy
+    @grm_dog = GrmDog.find(params[:id])
+    @grm_dog.destroy
+
+    respond_to do |format|
+      format.html { redirect_to grm_dogs_url }
+      format.json { head :no_content }
+    end
   end
 end
